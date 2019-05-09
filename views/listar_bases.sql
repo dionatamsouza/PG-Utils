dcbc11583f59902c242201bb0eb9945f98d55f44
dionatamsouza <dionatamsouza@yahoo.com.br>
2019-05-09 01:15:42 -0300

    Update listar_bases.sql

--- views/listar_bases.sql
+++ views/listar_bases.sql
@@ -5,3 +5,12 @@
          , pg_database_size( pg_database.datname )                     AS tamanho_bytes
       FROM pg_catalog.pg_database
   ORDER BY pg_database_size( pg_database.datname ) DESC ;
+
+/*
+       SELECT pg_user.usename                                           AS owner
+          , pg_database.datname                                         AS banco
+          , pg_size_pretty( pg_database_size( pg_database.datname ) )   AS tamanho
+       FROM pg_database
+       JOIN pg_user
+         ON pg_user.usesysid = pg_database.datdba
+*/
