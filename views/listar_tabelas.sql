51c6431eddc0980d37b87ff74a756ec241bce25d
dionatamsouza <dionatamsouza@yahoo.com.br>
2019-05-08 21:08:05 -0300

    Create listar_tabelas.sql

---
+++ views/listar_tabelas.sql
@@ -0,0 +1,13 @@
+    CREATE VIEW "listar_tabelas" AS 
+          
+    SELECT T.schemaname                                                                               AS schemaname
+         , T.tablename                                                                                AS tablename
+         , pg_size_pretty( pg_relation_size( T.schemaname || '.' || T.tablename ) :: BIGINT )         AS tamanho_virtual
+         , pg_size_pretty( pg_total_relation_size( T.schemaname || '.' || T.tablename ) :: BIGINT )   AS tamanho_fisico
+         , pg_total_relation_size( T.schemaname || '.' || T.tablename ) :: BIGINT                     AS tamanho_fisico_bytes
+      FROM ( SELECT quote_ident ( pg_tables.schemaname ) AS schemaname
+                  , quote_ident ( pg_tables.tablename  ) AS tablename
+               FROM pg_catalog.pg_tables
+              WHERE pg_tables.schemaname !~ '^pg_'
+                AND pg_tables.schemaname != 'information_schema'
+           ) AS t ;
