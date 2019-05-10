0a4f204b489563edc75978573866966c70042648
dionatamsouza <dionatamsouza@yahoo.com.br>
2019-05-10 10:24:36 -0300

    Update listar_bases.sql

--- views/listar_bases.sql
+++ views/listar_bases.sql
@@ -1,11 +1,9 @@
     CREATE VIEW "listar_bases" AS
           
-       SELECT pg_user.usename                                           AS owner
-          , pg_database.datname                                         AS banco
-          , pg_database_size( pg_database.datname )                     AS tamanho_bytes
-          , pg_size_pretty( pg_database_size( pg_database.datname ) )   AS tamanho
-       FROM pg_database
-  LEFT JOIN pg_user
-         ON pg_user.usesysid = pg_database.datdba ;
-
-
+    SELECT pg_user.usename                                             AS owner
+         , pg_database.datname                                         AS banco
+         , pg_database_size( pg_database.datname )                     AS tamanho_bytes
+         , pg_size_pretty( pg_database_size( pg_database.datname ) )   AS tamanho
+      FROM pg_catalog.pg_database
+ LEFT JOIN pg_catalog.pg_user
+        ON pg_user.usesysid = pg_database.datdba ;
