c2a144b961f817f1060de8d409eca4f63acdb80d
dionatamsouza <dionatamsouza@yahoo.com.br>
2019-06-27 20:14:33 -0300

    Update install.sql

--- install.sql
+++ install.sql
@@ -1,3 +1,10 @@
+/**
+    * Creates views and procedures useful in a Postgres version 9.3.
+    
+    * @link http://www.dionatan.com.br
+    * @author Developer: Dionatan Pinto de Souza
+*/
+
 SET client_encoding = 'SQL_ASCII' ;
 
 CREATE SCHEMA IF NOT EXISTS "utils" ;
