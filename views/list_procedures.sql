6a55683babe1bfb87d4620b10cf5b6272b88e284
dionatamsouza <dionatamsouza@yahoo.com.br>
2019-06-27 20:21:31 -0300

    Update list_procedures.sql

--- views/list_procedures.sql
+++ views/list_procedures.sql
@@ -1,3 +1,10 @@
+/**
+    * List procedural languages.
+    
+    * @link http://www.dionatan.com.br
+    * @author Developer: Dionatan Pinto de Souza
+*/
+
     CREATE VIEW "utils"."list_procedures" AS
           
     SELECT DISTINCT
