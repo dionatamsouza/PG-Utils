ae90ad1f97c9eb2b0da313d63b11d3d34a970b23
dionatamsouza <dionatamsouza@yahoo.com.br>
2019-06-27 20:15:39 -0300

    Update uninstall.sql

--- uninstall.sql
+++ uninstall.sql
@@ -1,2 +1,9 @@
+/**
+    * Drop views and procedures.
+    
+    * @link http://www.dionatan.com.br
+    * @author Developer: Dionatan Pinto de Souza
+*/
+
 -- Dropar o Schema contendo as views e procedures da ferramenta.
 DROP SCHEMA IF EXISTS "utils" CASCADE ;
