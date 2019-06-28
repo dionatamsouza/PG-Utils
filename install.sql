c35c3af3e8d209ed808ef8b27731306bd2fb4213
dionatamsouza <dionatamsouza@yahoo.com.br>
2019-06-28 09:46:58 -0300

    Update install.sql

--- install.sql
+++ install.sql
@@ -8,3 +8,13 @@
 SET client_encoding = 'SQL_ASCII' ;
 
 CREATE SCHEMA IF NOT EXISTS "utils" ;
+
+./procedures/retrieve_dependency.plsql
+./views/list_database.sql
+./views/list_schemas.sql
+./views/list_tables.sql
+./views/list_columns.sql
+./views/list_indexes.sql
+./views/list_procedures.sql
+./views/list_relationships.sql
+./views/list_queries.sql
