551acf42f8aceb1d404e6b62ad5aa18cd08efd56
dionatamsouza <dionatamsouza@yahoo.com.br>
2019-07-12 13:42:46 -0300

    Update list_indexes.sql
    Added columns tablespace and is_clustered.

--- views/list_indexes.sql
+++ views/list_indexes.sql
@@ -7,8 +7,33 @@
 
     CREATE VIEW "utils"."list_indexes" AS
           
-    SELECT schemaname
-         , tablename
-         , indexname
-         , indexdef
-      FROM pg_catalog.pg_indexes ;
+    WITH all_clustered_tables_in_database AS (
+          
+    SELECT pg_namespace.nspname AS schemaname
+         , pg_class.relname     AS tablename
+         , pg_class2.relname    AS indexname
+      FROM pg_catalog.pg_index
+      JOIN pg_catalog.pg_class
+        ON pg_index.indrelid = pg_class.oid
+      JOIN pg_catalog.pg_namespace
+        ON pg_class.relnamespace = pg_namespace.oid
+      JOIN pg_catalog.pg_class AS pg_class2
+        ON pg_index.indexrelid = pg_class2.oid
+     WHERE pg_class.relkind = 'r'
+       AND pg_class.relhasindex    IS TRUE
+       AND pg_index.indisclustered IS TRUE
+          
+    )
+    
+    SELECT pg_indexes.tablespace   AS tablespace
+         , pg_indexes.schemaname   AS schemaname
+         , pg_indexes.tablename    AS tablename
+         , pg_indexes.indexname    AS index_name
+         , pg_indexes.indexdef     AS index_definition
+         , CASE WHEN ct.indexname IS NULL THEN FALSE ELSE TRUE END AS is_clustered
+          
+      FROM pg_catalog.pg_indexes
+ LEFT JOIN all_clustered_tables_in_database AS ct
+        ON pg_indexes.schemaname = ct.schemaname
+       AND pg_indexes.tablename  = ct.tablename
+       AND pg_indexes.indexname  = ct.indexname ;
