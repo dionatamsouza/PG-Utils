c8e82c7e2626559292ea0e34b81a9d735f4ae25b
dionatamsouza <dionatamsouza@yahoo.com.br>
2019-08-02 14:00:49 -0300

    Create list_views.sql

---
+++ views/list_views.sql
@@ -0,0 +1,20 @@
+/**
+    * List views and materialized views.
+    
+    * @link http://www.dionatan.com.br
+    * @author Developer: Dionatan Pinto de Souza
+*/
+
+    CREATE VIEW "utils"."list_views" AS 
+
+    SELECT pg_namespace.nspname   AS schemaname
+         , pg_class.relname       AS viewname
+         , CASE pg_class.relkind
+               WHEN 'v' THEN false
+               WHEN 'm' THEN true
+           END AS materialized
+         , pg_get_viewdef(pg_class.oid)   AS source
+      FROM pg_catalog.pg_class
+ LEFT JOIN pg_catalog.pg_namespace
+        ON pg_namespace.oid = pg_class.relnamespace
+     WHERE pg_class.relkind IN ( 'v', 'm' ) ;
