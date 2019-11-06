8c9f364ab9346f1e71db6fb09503244570a01e33
dionatamsouza <dionatamsouza@yahoo.com.br>
2019-11-06 17:56:14 -0300

    Create list_triggers.sql

---
+++ views/list_triggers.sql
@@ -0,0 +1,18 @@
+/**
+    * List tables and their triggers.
+    
+    * @link http://www.dionatan.com.br
+    * @author Developer: Dionatan Pinto de Souza
+*/
+
+    CREATE VIEW "utils"."list_triggers" AS
+
+    SELECT pg_namespace.nspname   AS schemaname
+         , pg_class.relname       AS tablename
+         , pg_trigger.tgname      AS trigger
+      FROM pg_catalog.pg_trigger
+      JOIN pg_catalog.pg_class
+        ON pg_trigger.tgrelid = pg_class.oid
+      JOIN pg_catalog.pg_namespace
+        ON pg_class.relnamespace = pg_namespace.oid
+     WHERE pg_trigger.tgname !~ '^[A-Z]*\_ConstraintTrigger\_[a-z]*\_[0-9]*$' ;
